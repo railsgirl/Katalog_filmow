4e0163a7767b3aff4b1824f6860f8486ba6d67e0
Paulina <paulina.recko@gmail.com>
2018-06-26 16:22:02 +0200

    Routes,controllers, views

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,55 @@
+class MoviesController < ApplicationController
+    def index
+        @movies = Movie.all
+    end
+        
+    def show
+    end
+
+    def new
+        @movie = Movie.new
+    end
+
+    def create
+        @movie = Movie.new(movie_params)
+        if @movie.save
+           redirect_to @movie, notice: 'The movie has been added!'
+        else 
+            render 'new'
+        end 
+    end
+    
+    def edit 
+    end
+        
+    def update
+        if @movie.update(movie_params)
+            redirect_to @movie, notice: 'Update successful'
+        else
+            render 'edit'
+        end
+    end
+        
+    def destroy
+       @movie.destroy
+        redirect_to root_path, notice: 'Movie deleted'
+    end
+        
+    private
+        def movie_params
+            params.require(:movie).permit(:title, :genre, :length, :premiere_date, 
+              :director, :scriptwriter, :description)
+        end
+            
+        def find_movie
+            @movie = Movie.find(params[:id])
+        end
+        
+             #def check_movie_access
+            #return if @movie.user == admin
+            #redirect_to articles_path
+           #end
+           
+    end   
+        
+
